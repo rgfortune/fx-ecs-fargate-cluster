0ea7b5d34f57b9a7081a982351babab528c61750
Ricardo Fortune <ricardo.g.fortune@gmail.com>
2022-06-05 16:28:52 +0100

    Fix tagging of security groups - add variable declarations

--- environments/test/security_groups/variables.tf
+++ environments/test/security_groups/variables.tf
@@ -5,4 +5,16 @@ variable "region" {
   type        = string
   description = "Region"
 }
+variable "owner" {
+  type        = string
+  description = "e.g. John Public"
+}
+variable "project" {
+  type        = string
+  description = "e.g. Demo"
+}
+variable "environment" {
+  type        = string
+  description = "Environment"
+}
 
