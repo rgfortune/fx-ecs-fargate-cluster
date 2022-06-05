dfc607b955c495b879ba08e05280bdd3df6fa7b3
Ricardo Fortune <ricardo.g.fortune@gmail.com>
2022-06-05 17:35:06 +0100

    Declare IAM remote state data for bastion module

--- blueprints/bastion/sources.tf
+++ blueprints/bastion/sources.tf
@@ -63,3 +63,20 @@ data "terraform_remote_state" "security_groups" {
   # https://www.terraform.io/docs/language/functions/file.html
 
 }
+
+data "terraform_remote_state" "iam" {
+  backend = "s3"
+
+  # Paths are relative to the calling/root module.
+  # {path.module} - making the path relative to the module where the expression exists.
+
+  config = {
+    bucket = "ecs-fx-test-tfstate"
+    key    = "ecs-fx-iam.terraform.tfstate"
+    region = "us-east-1"
+  }
+
+  # https://www.terraform.io/docs/language/expressions/references.html#path-module
+  # https://www.terraform.io/docs/language/functions/file.html
+
+}
