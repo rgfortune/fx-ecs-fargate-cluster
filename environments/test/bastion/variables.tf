3c5d2578db010ea2788581d04b75fd97c72e0082
Ricardo Fortune <ricardo.g.fortune@gmail.com>
2022-06-05 16:47:40 +0100

    Fix tagging of bastion instance

--- environments/test/bastion/variables.tf
+++ environments/test/bastion/variables.tf
@@ -2,6 +2,22 @@
 # Variables
 #------------------------------------------------
 
+variable "region" {
+  type        = string
+  description = "Region"
+}
+variable "owner" {
+  type        = string
+  description = "e.g. John Public"
+}
+variable "project" {
+  type        = string
+  description = "e.g. Demo"
+}
+variable "environment" {
+  type        = string
+  description = "Environment"
+}
 variable "bastion_type" {
   type = string
 }
