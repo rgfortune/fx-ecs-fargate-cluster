adf662fe7b074b99be266e3ded0403bf9298818c
Ricardo Fortune <ricardo.g.fortune@gmail.com>
2022-06-05 17:27:44 +0100

    Rename bastion profile resources

--- blueprints/iam/outputs.tf
+++ blueprints/iam/outputs.tf
@@ -4,4 +4,4 @@
 
 output "ecs_task_execution_role_arn" { value = aws_iam_role.ecs_task_execution_role.arn }
 output "ecs_task_role_arn" { value = aws_iam_role.ecs_task_role.arn }
-output "bastion_role_arn" { value = aws_iam_role.bastion.arn }
+output "bastion_role_arn" { value = aws_iam_role.fx_bastion.arn }
