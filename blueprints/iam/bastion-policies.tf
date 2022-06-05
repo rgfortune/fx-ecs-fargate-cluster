164e63f65f2c85c98416af38e545e2038a3a3ec0
Ricardo Fortune <ricardo.g.fortune@gmail.com>
2022-06-05 17:06:57 +0100

    Add bastion instance profile

---
+++ blueprints/iam/bastion-policies.tf
@@ -0,0 +1,57 @@
+#------------------------------------------
+# Bastion Role
+#------------------------------------------
+
+resource "aws_iam_role" "bastion" {
+  name = "Bastion"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ec2.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+# Bastion policy document
+data "aws_iam_policy_document" "bastion" {
+  statement {
+    sid    = "BastionAccess"
+    effect = "Allow"
+    actions = [
+      "s3:*",
+      "ssm:*"
+    ]
+    resources = ["*"]
+  }
+}
+
+# Create bastion IAM policy
+resource "aws_iam_policy" "bastion" {
+  name        = "bastion"
+  path        = "/"
+  description = "Bastion policy"
+  policy      = data.aws_iam_policy_document.bastion.json
+}
+
+resource "aws_iam_policy_attachment" "bastion" {
+  name       = "bastion"
+  roles      = [aws_iam_role.bastion.name]
+  policy_arn = aws_iam_policy.bastion.arn
+}
+
+resource "aws_iam_policy_attachment" "bastion" {
+  name       = "bastion"
+  roles      = [aws_iam_role.bastion.name]
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
