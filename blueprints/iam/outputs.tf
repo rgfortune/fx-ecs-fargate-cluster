fa7e6ce3b1a031e199dc557edaef1e09fa555b42
Ricardo Fortune <ricardo.g.fortune@gmail.com>
2022-06-05 17:10:08 +0100

    Add bastion instance profile

--- blueprints/iam/outputs.tf
+++ blueprints/iam/outputs.tf
@@ -4,3 +4,4 @@
 
 output "ecs_task_execution_role_arn" { value = aws_iam_role.ecs_task_execution_role.arn }
 output "ecs_task_role_arn" { value = aws_iam_role.ecs_task_role.arn }
+output "bastion_role_arn" { value = aws_iam_role.bastion.arn }
